9a32afc4dc35805e26121c0ab2b2e20be482fc4e
Rishav Sharma <rishishah907@gmail.com>
2021-11-12 16:24:16 +0530

    Made improvement in Total runs scored by teams

--- total_runs_scored_by_teams.py
+++ total_runs_scored_by_teams.py
@@ -1,26 +1,41 @@
 import csv
 import matplotlib.pyplot as plt
-with open('deliveries.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    s = 0
-    teams = []
-    total = []
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            # print(' '.join(row))
-            if row[2] in teams:
-                total[teams.index(row[2])]+=int(row[17])
+
+
+def runs_scored_by_teams():
+    with open('deliveries.csv') as deliveries_file:
+        match_reader = csv.reader(deliveries_file, delimiter=',')
+        
+        line_count = 0
+        s = 0
+        teams = []
+        total = []
+
+        # Reading each row of the csv file
+        for match in match_reader:
+            # Since first column contains field names, hence we are skipping it
+            if line_count == 0:
+                line_count += 1
             else:
-                teams.append(row[2])
-                total.append(int(row[17]))
-            line_count += 1
-    print(teams,total)
-    plt.plot(teams,total)
+                #Checking if team's name is present in team list
+                if match[2] in teams:
+                    total[teams.index(match[2])] += int(match[17])
+                else:
+                    teams.append(match[2])
+                    total.append(int(match[17]))
+                line_count += 1
+
+    plotting_graph(teams,total)
+
+
+def plotting_graph(teams, total):
+    plt.figure(figsize = (20, 10))
+    plt.bar(teams,total)
     plt.xlabel('Teams')
     plt.ylabel('Total Score')
+    plt.title("Total runs Scored by Teams")
     plt.show()
-    print(f'Processed {line_count} lines.')
+
+
+if __name__ == '__main__':
+    runs_scored_by_teams()
